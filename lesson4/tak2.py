a73fd57cd5a7e5eef82ffe78433c11c2db4ebb17
ruslanbeetr00t <39996778+ruslanbeetr00t@users.noreply.github.com>
2021-05-24 23:08:19 +0300

    Delete tak2.py
    delete

--- lesson4/tak2.py
+++
@@ -1,7 +0,0 @@
-print('pleas write you are number here:')
-number = input()
-if len(number) == 10:
-    print('You are number', number, 'correct!!!')
-else:
-    len(number) != 10
-    print(number,'ERROR')
