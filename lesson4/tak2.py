369bba556511aa4df557d13e77e7ab739a92fdab
пк <treshmetal.ua@gmail.com>
2021-05-24 22:58:28 +0300

    нарешті вистачило розуму виконати завдяння(мабуть)

---
+++ lesson4/tak2.py
@@ -0,0 +1,7 @@
+print('pleas write you are number here:')
+number = input()
+if len(number) == 10:
+    print('You are number', number, 'correct!!!')
+else:
+    len(number) != 10
+    print(number,'ERROR')
